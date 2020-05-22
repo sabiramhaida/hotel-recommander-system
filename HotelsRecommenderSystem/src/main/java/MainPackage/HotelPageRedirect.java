9a6650c9760277ceff234077c36a57b0cfc23a3f
Sabir Amhaida <sabir.amhaida@um5s.net.ma>
2020-05-22 05:22:27 +0000

    Update hotelpage serv
    update class to save user activity ( viewing an  hotel)

--- HotelsRecommenderSystem/src/main/java/MainPackage/HotelPageRedirect.java
+++ HotelsRecommenderSystem/src/main/java/MainPackage/HotelPageRedirect.java
@@ -1,12 +1,16 @@
 package MainPackage;
 
 import Dao.Hotel_Dao;
+import Dao.UserActivity_Dao;
 import Model.Hotel;
+import Model.UserActivity;
 import org.bson.types.ObjectId;
 
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpSession;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
@@ -19,22 +23,35 @@ public class HotelPageRedirect extends javax.servlet.http.HttpServlet {
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         try {
+            //--------------------- saving user Activity (viewing) ----------------------------
+            ObjectId hotelId = new ObjectId(request.getParameter("hotel_id").toString());
+            Hotel hotel = new Hotel_Dao().getHotel(hotelId);
+            String hotelName = hotel.getName(); //hotel name
+            HttpSession session = request.getSession();
+            String userName =  session.getAttribute("username").toString(); //user name
+            if(userName != null) {
+                String activityOfuser = "view"; // user activity = viewing
+                Date activityDate = new Date();
+                new UserActivity_Dao().insertUserActivity(userName, hotelName, activityOfuser,activityDate );
+            }
+            //----------------------------------------------------------------------------------------
+
             pythonCodeExecutor(request);
             request.getRequestDispatcher("hotelPage.jsp").forward(request, response);
         } catch (ClassNotFoundException | InterruptedException e) {
             e.printStackTrace();
         }
-
     }
 
     public void pythonCodeExecutor(javax.servlet.http.HttpServletRequest request) throws ClassNotFoundException, IOException, InterruptedException {
-
+        //
         System.out.println("Python started");
         ObjectId hotelId = new ObjectId(request.getParameter("hotel_id").toString());
         Hotel hotel = new Hotel_Dao().getHotel(hotelId);
         request.setAttribute("hotel", hotel);
         System.out.println("Hotel name : " + hotel.getName());
-        String path = "/home/aym/Documents/gitHub/hotel-recommander-system/HotelsRecommenderSystem/src/main/webapp/hotel_data_cleaning_and_preprcessing.py";
+        String path = "/home/sabir/projects/Github/hotels-recommander-system/HotelsRecommenderSystem/src/main/webapp/hotel_data_cleaning_and_preprcessing.py";
+        System.out.println(path);
         File file = new File(path);
 
         List<String> list = new ArrayList<String>();
